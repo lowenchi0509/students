caa154915a632d871af2360e6ab5d28edfefcd7c
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-22 11:59:53 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -114,6 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
